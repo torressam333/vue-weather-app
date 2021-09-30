2515fcd59ec573495323917c8e73148fecb11c7b
Samuel <torressam333@gmail.com>
2021-09-30 08:00:35 -0700

    Initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+header {
+  align-items: center;
+  background-color: #3071a4;
+  display: flex;
+  height: 5rem;
+  justify-content: center;
+  width: 100%;
+}
+
+header h1 {
+  color: white;
+  margin: 0;
+}
+</style>
